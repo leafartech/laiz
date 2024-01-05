9c0f901773d08365d49c0632a4ed3ea5c8449edd
leafartech <119262053+leafartech@users.noreply.github.com>
2024-01-05 15:13:20 -0300

    Update index.tsx

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 export default function Button({ children }: ButtonProps) {
     return (
         <div className="w-full flex justify-center items-center">
-            <a href="" className="text-white font-semibold py-3 w-full sm:w-96 text-sm sm:text-lg bg-emerald-500 rounded-md text-center">{children}</a>
+            <a href="#_forms_3" className="text-white font-semibold py-3 w-full sm:w-96 text-sm sm:text-lg bg-emerald-500 rounded-md text-center">{children}</a>
         </div>
     )
 }
